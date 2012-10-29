73fe517573de2fd3a30fafa0d037ca132da2e127
ARNO* <arno@rezo.net>
2012-10-29 15:53:36 +0000

    Mise à jour foirée: le champ UUID vide se repère par "uuid IS NULL"
    git-svn-id: svn://trac.rezo.net/seenthis/plugin_seenthis_principal@250 726c7f6e-f4ed-4363-883b-90581dc1cf32

--- inc/seenthis_uuid.php
+++ inc/seenthis_uuid.php
@@ -46,8 +46,9 @@ function get_create_me_uuid($what=null, $id_auteur=null) {
 
 function seenthis_remplir_uuid() {
 	spip_log("remplir arbitrairement les uuid manquants", 'maj');
+	echo "TEST";
 	include_spip('inc/uuid');
-	$s = sql_query('SELECT id_me FROM spip_me WHERE uuid=""');
+	$s = sql_query('SELECT id_me FROM spip_me WHERE uuid IS NULL');
 	while ($t = sql_fetch($s)) {
 		$uuid = UUID::getuuid();
 		sql_updateq('spip_me', array('uuid'=>$uuid), 'id_me='.$t['id_me']);
