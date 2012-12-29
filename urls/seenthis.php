55dc625a8ed8189d05e1ecd4f19f9168a20525b7
Fil <fil@rezo.net>
2012-12-29 23:24:48 +0000

    bonne gestion des parametres dans l'URL (ex: tag/666?follow=fil vs tag/666)
    git-svn-id: svn://trac.rezo.net/seenthis/plugin_seenthis_principal@286 726c7f6e-f4ed-4363-883b-90581dc1cf32

--- urls/seenthis.php
+++ urls/seenthis.php
@@ -160,15 +160,14 @@ function urls_seenthis_dist($i, &$entite, $args='', $ancre='') {
 				$tag = substr($tag,0,-5);
 				$fond = 'backend_mot';
 			}
-
-			$contexte = array('tag' => $tag);
+			$args['tag'] = $tag;
 
 			/* old style = id_mot */
 			if ($f = sql_fetsel('m.id_mot AS id_mot', 'spip_mots AS m LEFT JOIN spip_groupes_mots AS g ON m.id_groupe=g.id_groupe', 'm.titre='.sql_quote($titre).' AND g.titre='.sql_quote($type))) {
-				$contexte['id_mot'] = $f['id_mot'];
+				$args['id_mot'] = $f['id_mot'];
 
 			$g = array(
-				$contexte,
+				$args,
 				$fond,
 				null,
 				null
