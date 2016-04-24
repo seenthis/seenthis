bf3f607b6486eecf7b34895d6f5a2b888b83bc21
brunob <brunobergot@gmail.com>
2016-04-24 15:06:20 +0200

    paquet.xml + maj spip 3 + rangement
    - plugin.xml to paquet.xml, compat SPIP 3 mini, categorie divers
    - ménage dans le fichier base/seenthis qui contenait un peu tout et n'importe quoi
    - ajout d'un fichier _fonctions pour la balise texte
    - méhtode d'install/upgrade/désinstall à la SPIP 3

--- base/seenthis.php
+++ base/seenthis.php
@@ -26,23 +26,7 @@ function seenthis_lire_create_table($x) {
 	return $m;
 }
 
-
-function balise_TEXTE ($p) {
-	if (in_array($p->type_requete, array('spip_me', 'me'))) {
-		$_id_me = champ_sql('id_me', $p);
-		$p->code = "texte_de_me($_id_me)";
-		$p->interdire_scripts = true;
-		return $p;
-	}
-	else {
-		$f = charger_fonction('DEFAUT', 'calculer_balise');
-		return $f('TEXTE', $p);
-	}
-}
-
-
 function seenthis_declarer_tables_interfaces($interface){
-
 	// 'spip_' dans l'index de $tables_principales
 	$interface['table_des_tables']['me']='me';
 	$interface['table_des_tables']['me_texte']='me_texte';
@@ -62,7 +46,7 @@ function seenthis_declarer_tables_objets_surnoms($interface){
 	$interface['me_texte']='me_texte';
 	$interface['me_recherche']='me_recherche';
 	$interface['me_follow']='me_follow';
-		
+
 	return $interface;
 }
 
@@ -248,135 +232,6 @@ function seenthis_declarer_tables_auxiliaires($tables_auxiliaires){
 
 }
 
-function seenthis_upgrade($nom_meta_base_version,$version_cible){
-	$current_version = 0.0;
-	if ((!isset($GLOBALS['meta'][$nom_meta_base_version]) )
-	|| (($current_version = $GLOBALS['meta'][$nom_meta_base_version])!=$version_cible)){
-		include_spip('base/abstract_sql');
-		if (version_compare($current_version,"1.1.8",'<')){
-			include_spip('base/serial');
-			include_spip('base/auxiliaires');
-			include_spip('base/create');
-			creer_base();
-
-			maj_tables(array(
-				'spip_auteurs',
-				'spip_me',
-				'spip_me_texte',
-				'spip_me_recherche',
-				'spip_me_auteur',
-				'spip_me_follow',
-				'spip_me_block',
-				'spip_me_follow_url',
-				'spip_me_share',
-				'spip_me_syndic',
-				'spip_syndic',
-				'spip_traductions',
-				'spip_me_tags'
-			));
-
-
-			// en 0.9.8, remplir arbitrairement les uuid manquants
-			if (version_compare($current_version,"0.9.8",'<')){
-				include_spip('inc/seenthis_uuid');
-				seenthis_remplir_uuid();
-			}
-
-			// en 0.9.9, remplir spip_me_tags & spip_me_follow_tags
-			if (version_compare($current_version,"0.9.9",'<')){
-				seenthis_mots2tags();
-			}
-
-			// en 1.0.1, remplir spip_me_recherche.titre
-			if (version_compare($current_version,"1.0.1",'<')){
-				seenthis_maj_recherche_titre();
-			}
-
-			// en 1.0.2, supprimer les tables de mots
-			if (version_compare($current_version,"1.0.2",'<')){
-				sql_drop_table("spip_me_follow_mot");
-				sql_drop_table("spip_me_mot");
-			}
-			// en 1.1.1, ajouter champs rezosocio
-			if (version_compare($current_version,"1.1.1",'<')){
-				sql_alter("TABLE spip_auteurs ADD twitter varchar(100) DEFAULT '' NOT NULL");
-				sql_alter("TABLE spip_auteurs ADD facebook varchar(100) DEFAULT '' NOT NULL");
-				sql_alter("TABLE spip_auteurs ADD gplus varchar(100) DEFAULT '' NOT NULL");
-			}
-			// en 1.1.2, ajouter l'option "mail a mes propres billets"
-			// gere par maj_tables()
-			#if (version_compare($current_version,"1.1.2",'<')){}
-
-			// en 1.1.3, ajouter la key(id_me) sur spip_me_tags
-			#if (version_compare($current_version,"1.1.3",'<')){}
-
-			// en 1.1.4, poser mail_partage=0 (c'est nouveau)
-			// et mail_rep_partage = mail_rep_moi (c'était confondu)
-			// cf. https://github.com/seenthis/seenthis/pull/7
-			if (version_compare($current_version,"1.1.4",'<')){
-				sql_query("UPDATE spip_auteurs SET mail_partage=0, mail_rep_partage = mail_rep_moi");
-			}
-
-			// en 1.1.5, ajouter une clé unique sur spip_me_share en supprimant les doublons
-			if (version_compare($current_version,"1.1.5",'<')){
-				sql_query("ALTER IGNORE TABLE spip_me_share ADD UNIQUE INDEX spip_me_share_unique (id_me, id_auteur)");
-			}
-
-			// en 1.1.6, ajouter une clé sur spip_me_tags
-			if (version_compare($current_version,"1.1.6",'<')){
-				sql_query("ALTER TABLE spip_me_tags ADD INDEX spip_me_tags_index_tags (class, tag(255))");
-			}
-
-			// en 1.1.7, ajouter les colonne liens_partage_fb et liens_partage_tw
-			if (version_compare($current_version,"1.1.7",'<')){
-				sql_query("UPDATE spip_auteurs SET liens_partage_fb = 1, liens_partage_tw = 1");
-			}
-
-			// en 1.1.8, appliquer troll_forcer=0 pour les troll_forcer IS NULL
-			if (version_compare($current_version,"1.1.8",'<')){
-				sql_query("UPDATE spip_auteurs SET troll_forcer = 0 where troll_forcer IS NULL");
-			}
-
-			ecrire_meta($nom_meta_base_version,$current_version=$version_cible,'non');
-
-		}
-
-	}
-}
-
-function seenthis_vider_tables($nom_meta_base_version) {
-	effacer_meta($nom_meta_base_version);
-	sql_drop_table("spip_me");
-	sql_drop_table("spip_me_texte");
-	sql_drop_table("spip_me_recherche");
-	sql_drop_table("spip_me_auteur");
-	sql_drop_table("spip_me_follow");
-	sql_drop_table("spip_me_block");
-	sql_drop_table("spip_me_follow_url");
-	sql_drop_table("spip_me_share");
-	sql_drop_table("spip_me_syndic");
-	sql_drop_table("spip_traductions");
-}
-
-
-function seenthis_install($action,$prefix,$version_cible){
-	$version_base = $GLOBALS[$prefix."_base_version"];
-	switch ($action){
-		case 'test':
-			$ok = (isset($GLOBALS['meta'][$prefix."_base_version"])
-				AND version_compare($GLOBALS['meta'][$prefix."_base_version"],$version_cible,">="));
-			return $ok;
-			break;
-		case 'install':
-			seenthis_upgrade($prefix."_base_version",$version_cible);
-			break;
-		case 'uninstall':
-			seenthis_vider_tables($prefix."_base_version");
-			break;
-	}
-}
-
-
 // champs extras pour afficher tous les champs supplémentaires d'un auteur
 // dans l'espace privé (si on active le plugin champ_extras :
 // svn co svn://zone.spip.org/spip-zone/_plugins_/champs_extras_core/trunk champs_extras_core/
@@ -435,92 +290,3 @@ function seenthis_declarer_champs_extras($champs = array()){
 
 	return $champs;
 }
-
-
-function seenthis_mots2tags() {
-
-	# convertir les spip_me_mot+spip_mots+spip_groupes_mots
-	# => en spip_me_tags
-	$s = sql_query($a = "INSERT INTO spip_me_tags (id_me, uuid, tag, class, date, relevance, off)
-	SELECT
-		a.id_me,
-		a.uuid,
-		CONCAT(
-			CASE WHEN g.titre='Hashtags' THEN '#' ELSE CONCAT(g.titre,':') END,
-			m.titre
-		) as tag,
-		CASE WHEN g.titre='Hashtags' THEN '#' ELSE 'oc' END as class,
-		a.date,
-		am.relevance,
-		am.off
-	FROM
-		spip_me AS a
-		INNER JOIN spip_me_mot AS am ON a.id_me=am.id_me
-		INNER JOIN spip_mots AS m on m.id_mot = am.id_mot
-		INNER JOIN spip_groupes_mots AS g on m.id_groupe=g.id_groupe
-	");
-
-
-	# convertir les spip_me_syndic+spip_syndic
-	# => en spip_me_tags
-	$s = sql_query($a = "INSERT INTO spip_me_tags (id_me, uuid, tag, class, date)
-	SELECT
-		a.id_me,
-		a.uuid,
-		m.url_site as tag,
-		'url' as class,
-		a.date
-	FROM
-		spip_me AS a
-		INNER JOIN spip_me_syndic AS am ON a.id_me=am.id_me
-		INNER JOIN spip_syndic AS m on m.id_syndic = am.id_syndic
-	");
-
-	sql_query("ALTER TABLE spip_me_tags ADD INDEX `tag` (`tag`(60))");
-
-	# convertir spip_me_follow_mot(id_mot)
-	# => en spip_me_follow_tag("#spip")
-	$s = sql_query($a = "INSERT INTO spip_me_follow_tag (tag, id_follow, date)
-	SELECT
-		CONCAT(
-			CASE WHEN g.titre='Hashtags' THEN '#' ELSE CONCAT(g.titre,':') END,
-			m.titre
-		) as tag,
-		f.id_follow,
-		f.date
-	FROM
-		spip_mots AS m
-		INNER JOIN spip_me_follow_mot AS f ON m.id_mot=f.id_mot
-		LEFT JOIN spip_groupes_mots AS g ON m.id_groupe=g.id_groupe
-	");
-
-	# convertir spip_me_follow_url(id_url)
-	# => en spip_me_follow_tag("url")
-	$s = sql_query($a = "INSERT INTO spip_me_follow_tag (tag, id_follow, date)
-	SELECT
-		m.url_site as tag,
-		f.id_follow,
-		f.date
-	FROM
-		spip_syndic AS m
-		INNER JOIN spip_me_follow_url AS f ON m.id_syndic=f.id_syndic
-	");
-
-
-	sql_query("ALTER TABLE spip_me_follow_tag ADD INDEX `tag` (`tag`(60))");
-
-
-	// ajouter les md5 sur la table spip_syndic
-	sql_query("UPDATE spip_syndic SET `md5`=MD5(url_site) WHERE `md5` IS NULL");
-
-}
-
-function seenthis_maj_recherche_titre() {
-	$s = spip_query('SELECT id_me, texte FROM spip_me_recherche WHERE NOT (titre>"")');
-	while ($t = sql_fetch($s)) {
-		$titre = seenthis_titre_me($t['texte']);
-		sql_updateq('spip_me_recherche', array('titre' => $titre), 'id_me='.$t['id_me']);
-	}
-}
-
-?>
---
+++ lang/paquet-seenthis_fr.php
@@ -0,0 +1,19 @@
+<?php
+
+// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+
+// Fichier produit par PlugOnet
+// Module: paquet-seenthis
+// Langue: fr
+// Date: 22-04-2016 21:30:07
+// Items: 2
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// S
+	'seenthis_description' => 'Configuration de Seenthis',
+	'seenthis_slogan' => 'Configuration de Seenthis',
+);
+?>
