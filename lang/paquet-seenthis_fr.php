b13fc98b6db1069eed3a7d120abf881df6c9fa98
brunob <brunobergot@gmail.com>
2016-04-24 15:12:49 +0200

    commentaire inutile

--- lang/paquet-seenthis_fr.php
+++ lang/paquet-seenthis_fr.php
@@ -1,13 +1,5 @@
 <?php
 
-// Ceci est un fichier langue de SPIP -- This is a SPIP language file
-
-// Fichier produit par PlugOnet
-// Module: paquet-seenthis
-// Langue: fr
-// Date: 22-04-2016 21:30:07
-// Items: 2
-
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
@@ -16,4 +8,5 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'seenthis_description' => 'Configuration de Seenthis',
 	'seenthis_slogan' => 'Configuration de Seenthis',
 );
+
 ?>
