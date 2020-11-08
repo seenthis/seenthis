0132235a1310eb50013681db4f2530aab7965e80
brunob <bruno@eliaz.fr>
2020-11-08 14:42:27 +0100

    ménage

--- seenthis_pipelines.php
+++ seenthis_pipelines.php
@@ -8,9 +8,6 @@ function seenthis_afficher_contenu_objet($flux){
 	if ($objet == "auteur") {
 		$id_auteur = $flux["args"]["contexte"]["id_auteur"];	
 		$ret = recuperer_fond("prive/interface_bloquer_auteur", array("id_auteur"=>$id_auteur));
-		
-	
-	
 		$flux['data'] .= $ret;
 	}
 	
